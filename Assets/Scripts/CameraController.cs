98cf74336761b8170f4b44ab1e78af539a0946c4
fabri <i@gmail.commini@gmail.com>
2018-11-27 21:53:15 -0200

    11.27.2018_21.52
    -Added draw line script and collision detection calculation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerCameraController : NetworkBehaviour
+public class CameraController : NetworkBehaviour
 {
 
     private GameObject player;
